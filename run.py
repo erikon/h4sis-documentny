e717c901acddf38df44a2ffa4a091de794eadb4a
Eric Kong <eric.kong98@gmail.com>
2019-11-09 12:13:50 -0800

    first commit

---
+++ run.py
@@ -0,0 +1,41 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import pandas as pd
+import numpy as np
+
+
+class DocumentNY:
+    accout_sid = 'AC23af71ef368cbdd5eb39b7b01b3a87ba'
+    auth_token = '6807aa1a94669b7b544ae4a59e4847d6'
+    phone = '16072352907'
+    data = None
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello World"
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming messages"""
+
+    if DocumentNY.data is None:
+        DocumentNY.data = load_data()
+    print(DocumentNY.data)
+    input = request.form.getlist('Body')[0]
+    print(input)
+    # Start our response
+    resp = MessagingResponse()
+
+    # Return data as a response
+    resp.message("You texted us: '" + input + "'")
+
+    return str(resp)
+
+def load_data():
+    return pd.read_excel('./data/Wage theft all zipcodes NYC.xlsx')
+
+if __name__ == "__main__":
+    doc = DocumentNY()
+    app.run(port='8080', debug=True)
